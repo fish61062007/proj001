14b835db17b26a0869caf43a49f43c0766df1bd6
john <john@johns-macbook-pro.local>
2018-02-03 16:05:35 +0800

    123123qweqweqweqweqweqweqweqwe

---
+++ proj001/successSignup.swift
@@ -0,0 +1,38 @@
+//
+//  successSignup.swift
+//  proj001
+//
+//  Created by john on 2018/2/3.
+//  Copyright © 2018年 utaipei. All rights reserved.
+//
+
+import UIKit
+
+class successSignup: UIViewController {
+    @IBAction func backHome(_ sender: Any) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
